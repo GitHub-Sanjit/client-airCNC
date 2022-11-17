5f95a5a7fc8ca534a3154c67041b7fbf991761ef
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-18 03:37:01 +0600

    Conceptual Session part 1

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,7 +2,13 @@ import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
   // COnfig code goes here
-}
+  apiKey: "AIzaSyBj5ZtDcm2QNHP-ouhZ8J9qZbb9ouPdVCU",
+  authDomain: "aircnc-3a6f0.firebaseapp.com",
+  projectId: "aircnc-3a6f0",
+  storageBucket: "aircnc-3a6f0.appspot.com",
+  messagingSenderId: "196424611144",
+  appId: "1:196424611144:web:fecd1c39b046a404636f7d",
+};
 
 const app = initializeApp(firebaseConfig)
 
