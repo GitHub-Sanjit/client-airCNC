ff55844edf86f14c9bf8d6890f6aea32c21e15b9
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-20 19:31:13 +0600

    Conceptual Session AriCnC Part One Complete

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ExpCard from "../Components/Card/ExpCard";
+import HomeCard from "../Components/Card/HomeCard";
 import SearchForm from "../Components/Form/SearchForm";
+import Spinner from "../Components/Spinner/Spinner";
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allExp, setAllExp] = useState([]);
 
   useEffect(() => {
+    setLoading(true);
     fetch("expdata.json")
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setAllExp(data);
+        setLoading(false);
       });
   }, []);
   return (
@@ -18,8 +24,42 @@ const Home = () => {
         <SearchForm></SearchForm>
       </div>
       <div className="flex-1">
-        <div>Home Cards</div>
-        <div>ExpCards</div>
+        <div>
+          <div>
+            <div className="flex justify-between px-4 mt-10">
+              <p className="text-xl font-bold">Homes</p>
+              <Link to="/service-details">
+                <p>See All</p>
+              </Link>
+            </div>
+            <div className="container pb-8 pt-2 mx-auto">
+              <div className="flex flex-wrap">
+                {[...Array(3)].map((_, i) => (
+                  <HomeCard key={i}></HomeCard>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div>
+            <div className="flex justify-between px-4">
+              <p className="text-xl font-bold">Experiences</p>
+              <Link to="/coming-soon">
+                <p>See All</p>
+              </Link>
+            </div>
+            <div className="container pb-8 pt-2 mx-auto">
+              <div className="flex flex-wrap">
+                {allExp.slice(0, 4).map((exp, i) => (
+                  <ExpCard key={i} exp={exp}></ExpCard>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
